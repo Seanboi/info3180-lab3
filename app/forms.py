58df5e58182be608b325e2fe738c7216abf0235e
Sean Spence <seanspence2019@gmail.com>
2025-03-02 14:38:40 -0500

    Final files added

---
+++ app/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
+    subject = StringField('Subject', validators=[DataRequired()])
+    message = StringField('Message', validators=[DataRequired()])
+    
+    
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
 from app import app
 from flask import render_template, request, redirect, url_for, flash
+from app import mail
+from flask_mail import Message
+from app.forms import ContactForm
 
 
 ###
@@ -17,6 +20,19 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+@app.route('/contact',methods=['GET', 'POST'])
+def contact():
+    form = ContactForm()
+    if form.validate_on_submit():
+        msg = Message(form.subject.data,
+        sender=(request.form['name'],request.form['email'] ),
+        recipients=["to@example.com"])
+        msg.body = request.form['message']
+        mail.send(msg)
+        flash('Your email was sent successfully!')
+        return redirect('/')   
+    """Render website's contact form page."""
+    return render_template('contact.html',form=form)
 
 ###
 # The functions below should be applicable to all Flask apps.
